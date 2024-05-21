941c08c3c3ca40e272ad8ea9f6a7b1bb8d149c1c
Julia <julka.uujma@gmail.com>
2024-05-21 18:00:58 +0200

    edit lab2

--- lab2/script.js
+++ lab2/script.js
@@ -2,29 +2,73 @@
 
 const inputBox = document.querySelector("#input-box");
 const theList = document.querySelector("#unordered-list");
+const deleteDialog = document.querySelector("#delete-dialog");
+const taskContent = document.querySelector("#task-content");
+const confirmDelete = document.querySelector("#confirm-delete");
+const cancelDelete = document.querySelector("#cancel-delete");
+
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let currentDate = `${day}.${month}.${year}`;
+
+let taskToDelete;
 
 const addTask = () => {
   if (inputBox.value !== "") {
     const listItem = document.createElement("li");
     listItem.innerText = inputBox.value;
-    theList.append(listItem);
     const deleteTask = document.createElement("span");
-    deleteTask.innerText = "✕";
+    deleteTask.innerText = "x";
     listItem.append(deleteTask);
+    theList.append(listItem);
+    save();
   }
   inputBox.value = "";
-  save();
 };
 
 theList.addEventListener("click", (event) => {
   const target = event.target;
   if (target.tagName === "LI") {
     target.classList.toggle("checked");
+    if (target.classList.contains("checked")) {
+      const dateElement = document.createElement("div");
+      dateElement.className = "date-completed";
+      dateElement.innerText = `${currentDate}`;
+      target.appendChild(dateElement);
+    } else {
+      const dateElement = target.querySelector(".date-completed");
+      if (dateElement) {
+        dateElement.remove();
+      }
+    }
     save();
   } else if (target.tagName === "SPAN") {
-    target.parentElement.remove();
-    save();
+    taskToDelete = target.parentElement;
+    const dateElement = taskToDelete.querySelector(".date-completed");
+    if (dateElement) {
+      taskContent.innerText = taskToDelete.innerText.slice(
+        0,
+        -dateElement.innerText.length - 2
+      );
+    } else {
+      taskContent.innerText = taskToDelete.innerText.slice(0, -1);
+    }
+    deleteDialog.showModal();
   }
+});
+
+confirmDelete.addEventListener("click", () => {
+  if (taskToDelete) {
+    taskToDelete.remove();
+    save();
+    deleteDialog.close();
+  }
+});
+
+cancelDelete.addEventListener("click", () => {
+  deleteDialog.close();
 });
 
 const save = () => {
@@ -32,7 +76,10 @@ const save = () => {
 };
 
 const showTasks = () => {
-  theList.innerHTML = localStorage.getItem("task");
+  const storedTasks = localStorage.getItem("task");
+  if (storedTasks) {
+    theList.innerHTML = storedTasks;
+  }
 };
 
 showTasks();
