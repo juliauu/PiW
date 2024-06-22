a0bf9ddb23504c5a41570a476840ba266993085d
Julia <julka.uujma@gmail.com>
2024-06-22 18:12:45 +0200

    fix task search, add undo delete

--- lab2/script.js
+++ lab2/script.js
@@ -7,6 +7,7 @@ const deleteDialog = document.querySelector("#delete-dialog");
 const taskContent = document.querySelector("#task-content");
 const confirmDelete = document.querySelector("#confirm-delete");
 const cancelDelete = document.querySelector("#cancel-delete");
+const undoButton = document.querySelector("#undo-button");
 
 const date = new Date();
 let day = date.getDate();
@@ -15,31 +16,65 @@ let year = date.getFullYear();
 let currentDate = `${day}.${month}.${year}`;
 
 let taskToDelete;
+let deletedTasks = [];
+
+const createListItem = (text) => {
+  const listItem = document.createElement("li");
+  listItem.innerText = text;
+  const deleteTask = document.createElement("span");
+  deleteTask.innerText = "x";
+  listItem.append(deleteTask);
+  return listItem;
+};
 
 const addTask = () => {
   if (inputBox.value !== "") {
-    const listItem = document.createElement("li");
-    listItem.innerText = inputBox.value;
-    const deleteTask = document.createElement("span");
-    deleteTask.innerText = "x";
-    listItem.append(deleteTask);
+    const listItem = createListItem(inputBox.value);
     theList.append(listItem);
     save();
   }
   inputBox.value = "";
 };
 
+const restoreTask = () => {
+  if (deletedTasks.length > 0) {
+    const lastDeletedTask = deletedTasks.pop();
+    const listItem = createListItem(lastDeletedTask.content.trim());
+    theList.append(listItem);
+    deletedTasks = [];
+    save();
+  }
+};
+
 const filterTasks = () => {
-  const filter = searchBox.value.toLowerCase();
+  const filter = searchBox.value.trim().toLowerCase();
   const tasks = theList.getElementsByTagName("li");
+
   for (let i = 0; i < tasks.length; i++) {
     const task = tasks[i].innerText.toLowerCase();
-    if (task.includes(filter)) {
+
+    if (filter === "") {
       tasks[i].style.display = "";
     } else {
-      tasks[i].style.display = "none";
+      if (task.includes(filter)) {
+        tasks[i].style.display = "";
+      } else {
+        tasks[i].style.display = "none";
+      }
     }
   }
+};
+
+const moveToTrash = (task) => {
+  const dateElement = task.querySelector(".date-completed");
+  const deletedTask = {
+    content: dateElement
+      ? task.innerText.slice(0, -dateElement.innerText.length - 2)
+      : task.innerText.slice(0, -1),
+  };
+  deletedTasks.push(deletedTask);
+  task.remove();
+  save();
 };
 
 theList.addEventListener("click", (event) => {
@@ -75,9 +110,9 @@ theList.addEventListener("click", (event) => {
 
 confirmDelete.addEventListener("click", () => {
   if (taskToDelete) {
-    taskToDelete.remove();
-    save();
+    moveToTrash(taskToDelete);
     deleteDialog.close();
+    console.log(deletedTasks);
   }
 });
 
@@ -85,10 +120,15 @@ cancelDelete.addEventListener("click", () => {
   deleteDialog.close();
 });
 
+undoButton.addEventListener("click", () => {
+  restoreTask();
+});
+
 searchBox.addEventListener("input", filterTasks);
 
 const save = () => {
   localStorage.setItem("task", theList.innerHTML);
+  localStorage.setItem("deletedTasks", JSON.stringify(deletedTasks));
 };
 
 const showTasks = () => {
